a773b6e8e7d2068b2c15fb85de9996708ec10292
Hanny Kishawi <hkishawi@gmail.com>
2021-03-05 14:37:49 -0500

    use effect

--- my-recipe/src/App.js
+++ my-recipe/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from "react";
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const APP_ID = 'ea7129ef';
+  const APP_KEY = 'f6dce84ce20ee81c796598612db319fe';
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`;
+  
+
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+   console.log('Effect has been run'); 
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <form className='search-form'>
+        <input type='text'></input>
+        <button className='search-button' type='submit'>search</button>
+      </form>
+      <h1 onClick={() => setCounter(counter+1)}>{counter}</h1>
     </div>
-  );
+  ); 
 }
+
 
 export default App;
